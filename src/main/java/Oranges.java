747b2ce94c50942ea3f89a95364cf133e4d83529
Ruslan Ibragimov <ruselseo@gmail.com>
2024-06-02 23:50:54 +0600

    CustomArrayListTest completed, CustomArrayList remove method's size corrected

--- src/main/java/Oranges.java
+++ src/main/java/Oranges.java
@@ -8,9 +8,14 @@ public class Oranges implements Comparator<Oranges>{
         size = (int) (10 * Math.random()+1);
     }
 
+    public Oranges(int size) {
+        this.size = size;
+    }
+
     public int getSize() {
         return size;
     }
+
 
     @Override
     public String toString() {
@@ -38,41 +43,4 @@ public class Oranges implements Comparator<Oranges>{
         return Objects.hashCode(getSize());
     }
 
-
-//    public static int compareTo(Oranges o1, Oranges o2) {
-//        return o1.getSize() - o2.getSize();
-//
-//    }
-
-
-
-    public static void main(String[] args) {
-        Oranges orange1 = new Oranges();
-        Oranges orange2 = new Oranges();
-        Oranges orange3 = new Oranges();
-        Oranges orange4 = new Oranges();
-        Oranges orange5 = new Oranges();
-        Oranges orange6 = new Oranges();
-        Oranges orange7 = new Oranges();
-        Oranges orange8 = new Oranges();
-        System.out.println(orange1.getSize());
-        System.out.println(orange2.getSize());
-        CustomArrayList<Oranges> list = new CustomArrayList<>();
-        list.add(orange1);
-        list.add(orange2);
-        list.add(orange3);
-        list.add(orange4);
-        list.add(orange5);
-        list.add(orange6);
-        list.add(orange7);
-        list.add(orange8);
-        list.add(orange8);
-        list.add(orange8);
-        list.add(orange8);
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println(CustomQuickSort.quickSort(list));
-        CustomArrayList<Oranges> list1 = CustomQuickSort.quickSort(list);
-        System.out.println(list1);
-    }
 }
