a5f1078cc6860602a2c10ad6ac051a0ba9c4b732
Ruslan Ibragimov <ruselseo@gmail.com>
2024-06-03 12:32:30 +0600

    generated javadoc

---
+++ src/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CustomArrayList"},{"p":"<Unnamed>","l":"CustomArrayListTest"},{"p":"<Unnamed>","l":"CustomQuickSort"},{"p":"<Unnamed>","l":"CustomQuickSortTest"},{"p":"<Unnamed>","l":"Oranges"},{"p":"<Unnamed>","l":"OrangesTest"}];updateSearchResults();
